f1b01c793376e83e8fdcb08041b9991eba3df326
muzaffar.munir <muzaffar.munir@virtual-force.com>
2023-03-10 09:50:11 +0500

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginUserComponent } from './login-user/login-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: AppComponent,
+    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  {
+    path: 'login',
+    component: LoginUserComponent
+  },
+  {
+    path: '', redirectTo: 'dashboard', pathMatch: 'full'
+  }
+  
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
