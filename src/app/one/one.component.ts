03d9b6586d226166fa0d0064c475758de4306d0d
daedalusdigital-za <107707715+daedalusdigital-za@users.noreply.github.com>
2023-05-18 17:53:15 +0200

    profile page

--- src/app/one/one.component.ts
+++ src/app/one/one.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OneComponent implements OnInit {
 
+    // set the currenr year
+    year: number = new Date().getFullYear();
+    // Carousel navigation arrow show
+    showNavigationArrows: any;
+/**
+   * Confirm Otp Verification
+   */
+config = {
+  allowNumbersOnly: true,
+  length: 4,
+  isPasswordInput: false,
+  disableAutoFocus: false,
+  placeholder: '',
+  inputStyles: {
+    'width': '80px',
+    'height': '50px'
+  }
+};
   constructor() { }
 
   ngOnInit(): void {
