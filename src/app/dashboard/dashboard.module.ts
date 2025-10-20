a784f459dd2ff1177ed276718d7124b31fc0cbdb
Daedalus <107707715+daedalusdigital-za@users.noreply.github.com>
2025-10-20 12:08:04 +0200

    feat: Remove help page and navigation tab - Delete FAQ and Tutorials components, routes, and navigation links

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,8 +4,6 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardLayoutComponent } from './dashboard-layout/dashboard-layout.component';
-import { ListFaqsComponent } from './list-faqs/list-faqs.component';
-import { ListTutorialsComponent } from './list-tutorials/list-tutorials.component';
 import { ReportsComponent } from './reports.component';
 import { AuthDebugComponent } from './auth-debug.component';
 import { AddStatsComponent } from './add-stats/add-stats.component';
@@ -24,8 +22,6 @@ import { VerifyPhoneNumberComponent } from './verify-phone-number/verify-phone-n
   declarations: [
     DashboardComponent,
     DashboardLayoutComponent,
-    ListFaqsComponent,
-    ListTutorialsComponent,
     ReportsComponent,
     AuthDebugComponent,
     // AddReportComponent,
